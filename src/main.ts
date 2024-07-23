7c5fb1e61a1d8d43e58f8ff2b745ed7e910a8462
Camille <camille@alsacreations.fr>
2024-07-23 16:19:16 +0200

    installation projet

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import './style.css'
+import filmLogo from '/film.svg'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+//import { setupCounter } from './app.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://developer.themoviedb.org/docs/getting-started" target="_blank">
+      <img src="${filmLogo}" class="logo film" alt="Vite logo" />
+    </a>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Filmorama</h1>
+    <form class="card">
+      <label for="search">Recherchez un film</label>
+      <fieldset>
+        <input type="text" id="search" name="search" ></input>
+        <input type="submit" value="Recherchez"></input>
+      </fieldset>
+    </form>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+//setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ src/notes/notes.md
@@ -0,0 +1,26 @@
+# Notes
+
+## Requetes d'API
+
+### GET :hand:
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res) => res.json())
+    .then((json) => console.log(json)); 
+
+### POST :fist:
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId : 1,
+    })
+    }).then(res=>res.json()).then(json => console.log(json))
+
+#### Mettre en forme markdown
+
+:P [comprendre les requetes http](https://www.youtube.com/watch?v=o5qsUz2vnzg&list=RDCMUC5HDIVwuqoIuKKw-WbQ4CvA&start_radio=1)
+
+:pen: [mise en forme notes en markdown](https://www.markdownguide.org/basic-syntax/#code-blocks)
