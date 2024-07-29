ba48b8564352cff67288f477d33896c4e2face3b
Camille <camille@alsacreations.fr>
2024-07-29 15:54:02 +0200

    chore: détailler les consignes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-// export function setupCounter(element: HTMLButtonElement) {
-//   let counter = 0
-//   const setCounter = (count: number) => {
-//     counter = count
-//     element.innerHTML = `count is ${counter}`
-//   }
-//   element.addEventListener('click', () => setCounter(counter + 1))
-//   setCounter(0)
-// }
 
+/**
+ *  Étape 1: recuperer le texte compris dans le label
+ * 
+ *  Étape 2: utiliser l'api et récupérer des objets
+ * 
+ *  Étape 3: afficher les objets dans des cards dynamiques
+ * 
+ *  Étapes 4: créer un filtre qui permet de rechercher les bons objets dans l'API
+ * 
+ *  Étapes 5, réinitialiser le filtre
+ */
 
